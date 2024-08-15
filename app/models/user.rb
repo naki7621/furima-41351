9fb1d4f546a160b2f6e105afb83451d8380edb14
naki7621 <007beaaaar@gmail.com>
2024-08-15 16:53:29 +0900

    ユーザー管理機能実装版ver1

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   validates :nickname, presence: true
-  validates :password, presence: true
   validates :password_confirmation, presence: true
   validates :family_name, presence: true
   validates :first_name, presence: true
