c13a9882db4c2de6dd62cd5cf6e74e30420c8b19
naki7621 <007beaaaar@gmail.com>
2024-08-16 12:52:54 +0900

    状態と配送料が完成

--- db/migrate/20240816023410_create_items.rb
+++ db/migrate/20240816023410_create_items.rb
@@ -1,9 +1,11 @@
 class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
-      t.string   :title,        null: false
-      t.text     :text,         null: false
-      t.integer  :category_id,  null: false
+      t.string   :title,              null: false
+      t.text     :text,               null: false
+      t.integer  :category_id,        null: false
+      t.integer  :condition_id,       null: false
+      t.integer  :delivery_charge_id, null: false
       t.timestamps
     end
   end
