303cb81710348f9bf04c2663e7efc30da96bdaec
naki7621 <007beaaaar@gmail.com>
2024-08-16 15:26:25 +0900

    バリデーション完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,13 @@ class Item < ApplicationRecord
   belongs_to :days_to_ship
   belongs_to :delivery_charge
   belongs_to :area
+
+  validates :title, presence: true
+  validates :text, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :area_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, presence: true
 end
