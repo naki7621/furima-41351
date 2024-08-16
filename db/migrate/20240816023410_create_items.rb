6f7ab98c209cf661fec3e74541433ad341f4f050
naki7621 <007beaaaar@gmail.com>
2024-08-16 14:17:42 +0900

    都道府県と発送が完了

--- db/migrate/20240816023410_create_items.rb
+++ db/migrate/20240816023410_create_items.rb
@@ -6,6 +6,8 @@ class CreateItems < ActiveRecord::Migration[7.0]
       t.integer  :category_id,        null: false
       t.integer  :condition_id,       null: false
       t.integer  :delivery_charge_id, null: false
+      t.integer  :area_id,            null: false
+      t.integer  :days_to_ship_id,    null: false
       t.timestamps
     end
   end
