f3792870c62579c506a6cff744954649f7b0d4c3
naki7621 <007beaaaar@gmail.com>
2024-08-16 18:01:36 +0900

    プルダウン表示されない

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,26 @@
 class ItemsController < ApplicationController
+
   def index
+    @items = Item.order("created_at DESC")
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title,:text, :category_id, :condition_id, :delivery_charge_id, :area_id, :days_to_ship_id, :price)
+  end
+
 end
