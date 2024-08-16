9f4cbaf21bc5405285c8a4d17c164869b408e5bd
naki7621 <007beaaaar@gmail.com>
2024-08-16 14:55:31 +0900

    アソシエーション完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :days_to_ship
+  belongs_to :delivery_charge
+  belongs_to :area
 end
