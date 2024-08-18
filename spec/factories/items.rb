7a62695e383fbc57d150b365b54513ff7c5d0edc
naki7621 <007beaaaar@gmail.com>
2024-08-18 15:02:12 +0900

    画像保存完了

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    a
+    image               {}
+    title               {'子犬'}
+    text                {'我が家の柴犬'}
+    category_id         { 2 }
+    condition_id        { 3 }
+    delivery_charge_id  { 2 }
+    area_id             { 11 }
+    days_to_ship_id     { 4 }
+    price               { 9999 }
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,31 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  describe '出品できないとき' do
-    it '画像がないと出品できない' do
-      item = Item.new(image: nil)
-      item.valid?
-      expect(item.errors.full_messages).to include("Image can't be blank")
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  describe '出品' do
+    context '出品できるとき' do
+      it '全ての必須項目が正しく入力されていれ出品できる' do
+        expect(@item).to be_valid
+      end
     end
-    it '商品名がないと出品できない' do
-    end
-    it '商品の説明がないと出品できない' do
-    end
-    it 'カテゴリーがないと出品できない' do
-    end
-    it '商品の状態がないと出品できない' do
-    end
-    it '配送料の負担がないと出品できない' do
-    end
-    it '発送元の地域がないと出品できない' do
-    end
-    it '発送までの日数がないと出品できない' do
-    end
-    it '価格がないと出品できない' do
-    end
-    it '価格が300円以下だと出品できない' do
-    end
-    it '価格が9,999,999円以上だと出品できない' do
+    context '出品できないとき' do
+      it '画像がないと出品できない' do
+      end
+      it '商品名がないと出品できない' do
+        @item.title = ''
+        @item.valid?
+        expect(item.errors.full_messages).to include("Title can't be blank")
+      end
+      it '商品の説明がないと出品できない' do
+      end
+      it 'カテゴリーがないと出品できない' do
+      end
+      it '商品の状態がないと出品できない' do
+      end
+      it '配送料の負担がないと出品できない' do
+      end
+      it '発送元の地域がないと出品できない' do
+      end
+      it '発送までの日数がないと出品できない' do
+      end
+      it '価格がないと出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(item.errors.full_messages).to include("Price can't be blank")
+      end
+      it '価格が300円以下だと出品できない' do
+        @item.price = '299'
+        @item.valid?
+        expect(item.errors.full_messages).to include("Price が300円以下では出品できない")
+      end
+      it '価格が9,999,999円以上だと出品できない' do
+      end
     end
   end
 end
