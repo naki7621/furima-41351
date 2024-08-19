978f6be88e68ee8039d80f3a65af341f4ab31697
naki7621 <007beaaaar@gmail.com>
2024-08-19 19:04:48 +0900

    商品一覧表示機能修正ver1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @items = Item.all
     @items = Item.order("created_at DESC")
   end
 
