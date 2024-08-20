a75835536367a93efd2e90aaed5d7bb1ac80c16b
naki7621 <007beaaaar@gmail.com>
2024-08-20 18:50:40 +0900

    テーブル修正

--- db/migrate/20240820092259_create_addresses.rb
+++ db/migrate/20240820092259_create_addresses.rb
@@ -5,10 +5,10 @@ class CreateAddresses < ActiveRecord::Migration[7.0]
       t.integer :area_id,             null: false
       t.string :city,                 null: false
       t.string :house_number,         null: false
-      t.string :house_number,         null: false
-      t.timestamps :building_name
-      t.timestamps :phone_number      null: false
+      t.string :building_name,        null: false
+      t.string :phone_number,         null: false
       t.references :order,            null: false, foreign_key: true
+      t.timestamps
     end
   end
 end
